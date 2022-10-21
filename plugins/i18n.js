a7de0299c0dad63807f525346afc0d081e9a0cef
Francis <magallen.fc@gmail.com>
2022-10-21 16:31:21 +0800

    Wip validation

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -4,9 +4,11 @@ import messages from '../utils/lang'
 
 Vue.use(VueI18n)
 
-const i18n = new VueI18n({
-  locale: 'en',
-  messages: messages,
-})
+console.log(messages)
 
-export { i18n }
+export default ({ app }) => {
+  app.i18n = new VueI18n({
+    locale: 'en',
+    messages: messages,
+  })
+}
--- plugins/validator.js
+++ plugins/validator.js
@@ -6,18 +6,13 @@ import {
   extend,
   configure,
 } from 'vee-validate'
-import { i18n } from './i18n'
-// import { first, isArray } from 'lodash'
 
-configure({
-  // @ts-ignore
-  defaultMessage: (field, values) => {
-    // override the field name.
-    values._field_ = i18n.t(`fields.${field}`)
-
-    return i18n.t(`validation.${values._rule_}`, values)
-  },
-})
+export default ({ app }) => {
+  configure({
+    defaultMessage: (_, values) =>
+      app.i18n.t(`validations.validation.${values._rule_}`, values),
+  })
+}
 
 // Add rules
 extend('required', {
@@ -33,64 +28,3 @@ extend('email', {
 // Global components
 Vue.component('ValidationObserver', ValidationObserver)
 Vue.component('ValidationProvider', ValidationProvider)
-
-// import Vue from 'vue'
-// import VeeValidate, { Validator } from 'vee-validate'
-// import { first, isArray } from 'lodash'
-
-// Vue.use(VeeValidate, {
-//   trigger: 'blur|input',
-// })
-
-// const dict = {
-//   custom: {
-//     email: {
-//       email: 'Please fill in a valid email address',
-//     },
-//     city: {
-//       required: () => 'Please fill in your city',
-//     },
-//   },
-// }
-
-// Validator.localize('en', dict)
-
-// export default ({ app }) => {
-//   Vue.prototype.$setErrorsFromResponse = function (errorResponse) {
-
-// only allow this function to be run if the validator exists
-// if (!this.$validator) {
-//   return
-// }
-
-// clear errors
-// this.$validator.errors.clear()
-
-// check if errors exist
-// if (!errorResponse.errors) {
-//   return
-// }
-
-// const errorFields = Object.keys(errorResponse.errors)
-
-// insert laravel errors
-// eslint-disable-next-line array-callback-return
-// errorFields.map((field) => {
-//   let error = errorResponse.errors[field]
-//   let msg = ''
-
-//   if (isArray(error)) {
-//     msg = first(error)
-//   } else {
-//     msg = error
-//   }
-
-//   this.$validator.errors.add({
-//     field,
-//     msg: app.i18n.tc(msg, null, null, {
-//       attribute: field,
-//     }),
-//   })
-// })
-// }
-// }
